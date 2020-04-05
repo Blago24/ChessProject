337bb2fbea7505037efabf90a5083fc3cf954a74
Blagovest Konetsovski <blago990@abv.bg>
2020-04-05 21:09:34 +0300

    King class refactored.Function getMoves separated.

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -39,84 +39,112 @@ public class King extends Piece {
 		else
 			return "k";
 	}
-
-	public ArrayList<Move> getMoves(Board b, int x, int y) {
-		ArrayList<Move> moves = new ArrayList<Move>();
-		
+	private void checkNorth(Board board,ArrayList<Move> moves,int x,int y){
 		// N
-		if(valid(x, y+1) && 
-				(!b.getTile(x, y+1).isOccupied() || 
-						(b.getTile(x, y+1).isOccupied() && b.getTile(x, y+1).getPiece().getColor() != color)))
+		if(valid(x, y+1) &&
+				(!board.getTile(x, y+1).isOccupied() ||
+						(board.getTile(x, y+1).isOccupied() && board.getTile(x, y+1).getPiece().getColor() != color)))
 			moves.add(new Move(x,y,x,y+1));
-		
-		// NE
-		if(valid(x+1, y+1) && 
-				(!b.getTile(x+1, y+1).isOccupied() || 
-						(b.getTile(x+1, y+1).isOccupied() && b.getTile(x+1, y+1).getPiece().getColor() != color)))
+
+	}
+	private void checkNorthEast(Board board,ArrayList<Move> moves,int x,int y){
+// NE
+		if(valid(x+1, y+1) &&
+				(!board.getTile(x+1, y+1).isOccupied() ||
+						(board.getTile(x+1, y+1).isOccupied() && board.getTile(x+1, y+1).getPiece().getColor() != color)))
 			moves.add(new Move(x,y,x+1,y+1));
-		
-		// E
-		if(valid(x+1,y) && 
-				(!b.getTile(x+1,y).isOccupied() || 
-						(b.getTile(x+1,y).isOccupied() && b.getTile(x+1,y).getPiece().getColor() != color)))
+
+	}
+	private void checkEast(Board board,ArrayList<Move> moves,int x,int y){
+// E
+		if(valid(x+1,y) &&
+				(!board.getTile(x+1,y).isOccupied() ||
+						(board.getTile(x+1,y).isOccupied() && board.getTile(x+1,y).getPiece().getColor() != color)))
 			moves.add(new Move(x,y,x+1,y));
-		
-		
-		// SE
-		if(valid(x+1,y-1) && 
-				(!b.getTile(x+1,y-1).isOccupied() || 
-						(b.getTile(x+1,y-1).isOccupied() && b.getTile(x+1,y-1).getPiece().getColor() != color)))
+
+	}
+	private void checkSouthEast(Board board,ArrayList<Move> moves,int x,int y){
+// SE
+		if(valid(x+1,y-1) &&
+				(!board.getTile(x+1,y-1).isOccupied() ||
+						(board.getTile(x+1,y-1).isOccupied() && board.getTile(x+1,y-1).getPiece().getColor() != color)))
 			moves.add(new Move(x,y,x+1,y-1));
-		
+
+	}
+	private void checkSouth(Board board,ArrayList<Move> moves,int x,int y){
 		// S
-		if(valid(x,y-1) && 
-				(!b.getTile(x,y-1).isOccupied() || 
-						(b.getTile(x,y-1).isOccupied() && b.getTile(x,y-1).getPiece().getColor() != color)))
+		if(valid(x,y-1) &&
+				(!board.getTile(x,y-1).isOccupied() ||
+						(board.getTile(x,y-1).isOccupied() && board.getTile(x,y-1).getPiece().getColor() != color)))
 			moves.add(new Move(x,y,x,y-1));
-		
+
+	}
+	private void checkSouthWest(Board board,ArrayList<Move> moves,int x,int y){
+
 		// SW
-		if(valid(x-1,y-1) && 
-				(!b.getTile(x-1,y-1).isOccupied() || 
-						(b.getTile(x-1,y-1).isOccupied() && b.getTile(x-1,y-1).getPiece().getColor() != color)))
+		if(valid(x-1,y-1) &&
+				(!board.getTile(x-1,y-1).isOccupied() ||
+						(board.getTile(x-1,y-1).isOccupied() && board.getTile(x-1,y-1).getPiece().getColor() != color)))
 			moves.add(new Move(x,y,x-1,y-1));
-		
+
+	}
+	private void checkWest(Board board,ArrayList<Move> moves,int x,int y){
 		// W
-		if(valid(x-1,y) && 
-				(!b.getTile(x-1,y).isOccupied() || 
-						(b.getTile(x-1,y).isOccupied() && b.getTile(x-1,y).getPiece().getColor() != color)))
+		if(valid(x-1,y) &&
+				(!board.getTile(x-1,y).isOccupied() ||
+						(board.getTile(x-1,y).isOccupied() && board.getTile(x-1,y).getPiece().getColor() != color)))
 			moves.add(new Move(x,y,x-1,y));
-		
-		// NW
-		if(valid(x-1,y+1) && 
-				(!b.getTile(x-1,y+1).isOccupied() || 
-						(b.getTile(x-1,y+1).isOccupied() && b.getTile(x-1,y+1).getPiece().getColor() != color)))
+
+	}
+	private void checkNorthWest(Board board,ArrayList<Move> moves,int x,int y){
+// NW
+		if(valid(x-1,y+1) &&
+				(!board.getTile(x-1,y+1).isOccupied() ||
+						(board.getTile(x-1,y+1).isOccupied() && board.getTile(x-1,y+1).getPiece().getColor() != color)))
 			moves.add(new Move(x,y,x-1,y+1));
 
-		// Castling
+	}
+	private void checkIfKingHasMoved(Board board,ArrayList<Move> moves,int x,int y){
 		if(color == Piece.WHITE) {
 			if(!hasMoved && x == Board.e && y == 1-1) {
-				if(!b.getTile(Board.f, 1-1).isOccupied() &&
-						!b.getTile(Board.g, 1-1).isOccupied() &&
-						b.getTile(Board.h, 1-1).isOccupied() && 
-						b.getTile(Board.h, 1-1).getPiece().toString().equals("R"))
+				if(!board.getTile(Board.f, 1-1).isOccupied() &&
+						!board.getTile(Board.g, 1-1).isOccupied() &&
+						board.getTile(Board.h, 1-1).isOccupied() &&
+						board.getTile(Board.h, 1-1).getPiece().toString().equals("R"))
 					moves.add(new Move(x,y,x+2,y));
-					
-						
+
+
 			}
-			else 
+			else
 				hasMoved = true;
 		}
 		else { // color == Piece.BLACK
 			if(!hasMoved && x == Board.e && y == 8-1) {
-				
+
 			}
-			else 
+			else
 				hasMoved = true;
 		}
-		
-		
+
+
 		// TODO King cannot move into open fire
+
+	}
+	public ArrayList<Move> getMoves(Board board, int x, int y) {
+		ArrayList<Move> moves = new ArrayList<Move>();
 		
+	checkNorth(board,moves,x,y);
+		checkNorthEast(board,moves,x,y);
+
+		checkEast(board,moves,x,y);
+		checkSouthEast(board,moves,x,y);
+		checkSouth(board,moves,x,y);
+		checkSouthWest(board,moves,x,y);
+		checkWest(board,moves,x,y);
+		checkNorthWest(board,moves,x,y);
+		// Castling
+		checkIfKingHasMoved(board,moves,x,y);
+
 		
 		return moves;
 	}
