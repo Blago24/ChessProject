938a0a7b61fd78a5e2d49d71db4e1a3a2fd426fe
Blagovest Konetsovski <blago990@abv.bg>
2020-04-05 21:20:55 +0300

    Bishop refactored. Function getMoves separated.

--- src/chess/pieces/Bishop.java
+++ src/chess/pieces/Bishop.java
@@ -30,66 +30,79 @@ public class Bishop extends Piece {
 		return new Bishop(color);
 	}
 
-	public ArrayList<Move> getMoves(Board b, int x, int y) {
-		ArrayList<Move> moves = new ArrayList<Move>();
-		
-		
-		// NE
+	private void checkNorthEast(Board board,ArrayList<Move> moves,int x,int y){
+// NE
 		for(int i = 1; i < 8; i++) {
 			if(valid(x+i, y+i)) {
-				if(b.getTile(x+i, y+i).isOccupied()) {
-					if(b.getTile(x+i, y+i).getPiece().color != color)
-						moves.add(new Move(x,y,x+i,y+i));	
-					
+				if(board.getTile(x+i, y+i).isOccupied()) {
+					if(board.getTile(x+i, y+i).getPiece().color != color)
+						moves.add(new Move(x,y,x+i,y+i));
+
 					break;
 				}
 				else
-					moves.add(new Move(x,y,x+i,y+i));	
+					moves.add(new Move(x,y,x+i,y+i));
 			}
 		}
-		
-		// NW
-		for(int i = 1; i < 8; i++) {
-			if(valid(x-i, y+i)) {
-				if(b.getTile(x-i, y+i).isOccupied()) {
-					if(b.getTile(x-i, y+i).getPiece().color != color)
-						moves.add(new Move(x,y,x-i,y+i));	
-					
-					break;
-				}
-				else
-					moves.add(new Move(x,y,x-i,y+i));	
-			}
-		}
-		
+
+	}
+
+	private void checkSouthEast(Board board,ArrayList<Move> moves,int x,int y){
+
 		// SE
 		for(int i = 1; i < 8; i++) {
 			if(valid(x+i, y-i)) {
-				if(b.getTile(x+i, y-i).isOccupied()) {
-					if(b.getTile(x+i, y-i).getPiece().color != color)
-						moves.add(new Move(x,y,x+i,y-i));	
-					
+				if(board.getTile(x+i, y-i).isOccupied()) {
+					if(board.getTile(x+i, y-i).getPiece().color != color)
+						moves.add(new Move(x,y,x+i,y-i));
+
 					break;
 				}
 				else
-					moves.add(new Move(x,y,x+i,y-i));	
+					moves.add(new Move(x,y,x+i,y-i));
 			}
 		}
-		
-		// SW
+	}
+
+	private void checkSouthWest(Board board,ArrayList<Move> moves,int x,int y){
+// SW
 		for(int i = 1; i < 8; i++) {
 			if(valid(x-i, y-i)) {
-				if(b.getTile(x-i, y-i).isOccupied()) {
-					if(b.getTile(x-i, y-i).getPiece().color != color)
-						moves.add(new Move(x,y,x-i,y-i));	
-					
+				if(board.getTile(x-i, y-i).isOccupied()) {
+					if(board.getTile(x-i, y-i).getPiece().color != color)
+						moves.add(new Move(x,y,x-i,y-i));
+
 					break;
 				}
 				else
-					moves.add(new Move(x,y,x-i,y-i));	
+					moves.add(new Move(x,y,x-i,y-i));
 			}
 		}
+	}
+
+	private void checkNorthWest(Board board,ArrayList<Move> moves,int x,int y){
+// NW
+		for(int i = 1; i < 8; i++) {
+			if(valid(x-i, y+i)) {
+				if(board.getTile(x-i, y+i).isOccupied()) {
+					if(board.getTile(x-i, y+i).getPiece().color != color)
+						moves.add(new Move(x,y,x-i,y+i));
+
+					break;
+				}
+				else
+					moves.add(new Move(x,y,x-i,y+i));
+			}
+		}
+
+	}
+	public ArrayList<Move> getMoves(Board board, int x, int y) {
+		ArrayList<Move> moves = new ArrayList<Move>();
 		
+		checkNorthEast(board,moves,x,y);
+		checkNorthWest(board,moves,x,y);
+		checkSouthEast(board,moves,x,y);
+		checkSouthWest(board,moves,x,y);
 		return moves;
 	}
 }
